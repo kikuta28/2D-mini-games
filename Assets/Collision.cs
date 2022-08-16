58f9f7f93ca985c106a0466d42cf25ad86c412ad
kikuta28 <knocknote.kikuta@gmail.com>
2022-08-16 17:53:11 +0900

    OnCollisionEnter2DのPrivate修飾子を削除

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D other)
+    void OnCollisionEnter2D(Collision2D other)
     {
 
     }
