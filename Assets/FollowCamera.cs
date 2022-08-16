25b21e93866b620bd7c35bdb51fccd26544d80ac
kikuta28 <knocknote.kikuta@gmail.com>
2022-08-17 07:02:21 +0900

    FollowCameraがCarと同じ位置ではなく, 上から見下ろすように

--- Assets/FollowCamera.cs
+++ Assets/FollowCamera.cs
@@ -7,6 +7,6 @@ public class FollowCamera : MonoBehaviour
   [SerializeField] GameObject targetToFollow;
     void Update()
     {
-        transform.position = targetToFollow.transform.position;
+        transform.position = targetToFollow.transform.position + new Vector3(0, 0, -10);
     }
 }
