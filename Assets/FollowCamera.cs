1310fd84ac32e58da75240761d944acd126c9a02
kikuta28 <knocknote.kikuta@gmail.com>
2022-08-17 06:24:49 +0900

    カメラがTargetToFollowの位置とおなじになる

--- Assets/FollowCamera.cs
+++ Assets/FollowCamera.cs
@@ -7,6 +7,6 @@ public class FollowCamera : MonoBehaviour
   [SerializeField] GameObject targetToFollow;
     void Update()
     {
-        
+        transform.position = targetToFollow.transform.position;
     }
 }
