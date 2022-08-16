23b4b9aa4acf4a0dd9234dda2ef6e9c00eea7073
kikuta28 <knocknote.kikuta@gmail.com>
2022-08-16 15:42:27 +0900

    RotateをUpdate内で試す

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -7,12 +7,12 @@ public class Driver : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.Rotate(0, 0, 45);
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.Rotate(0, 0, 45);
 
     }
 
